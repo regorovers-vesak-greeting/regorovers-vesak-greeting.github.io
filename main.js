c76353555889b026b53fd11714e5ce4c910d4c76
dinukolla <dinukolla@.github.com>
2021-05-24 21:28:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,11 +13,16 @@
     $('#nameModal').modal('show')
   }
 
+  String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  }
+
   view_card = () => {
 
-    let name = $('#name').val()
+    let name = $('#name').val().capitalize()
     $('.invalid-feedback').hide()
     if (name == '' || name == null){
+      
       $('#errname').html('<strong>Please Enter Your Name</strong>')
       $('.invalid-feedback').show()
     }else{      
