d1244a3849dcf8d7b4e176f6a7119c8a2cc0006e
dinukolla <dinukolla@.github.com>
2021-05-26 01:06:16 +0530

    audio

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 
   var video = document.getElementById("myVideo");
+  // var audio = document.getElementById("player");
  
   myFunction = () => {
     if (video.paused) {
       video.play();
     } else {
       video.pause();
+      // audio.pause();
     }
   }
+
+  // var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  // if (!isChrome){
+  //     $('#iframeAudio').remove()
+  // }
+  // else {
+  //     $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+  // }
 
   onload = () => {
     $('#nameModal').modal('show')
     // $('.content').removeClass('d-none')
+    // audio.play();
   }
 
   String.prototype.capitalize = function() {
@@ -23,15 +34,21 @@
     let name = $('#name').val().capitalize()
     $('.invalid-feedback').hide()
     if (name == '' || name == null){
-      
       $('#errname').html('<strong>Please Enter Your Name</strong>')
       $('.invalid-feedback').show()
     }else{      
+      play_audio()
       $('#toName').html(name)
 
       $('#nameModal').modal('hide')
     }
 
+  }
+
+  play_audio = () => {
+    var audio = document.getElementById("audio");
+    audio.currentTime=4
+    audio.play();
   }
 
   $(document).ready(function () {
